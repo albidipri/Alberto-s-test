40ef98cb842e0f359096e5f65884228e7e73c1d3
Alberto di Prisco <albertodiprisco@gmail.com>
2019-10-16 10:02:42 +0200

    First comment

---
+++ Martingale system.py
@@ -0,0 +1,48 @@
+import random
+import pandas as pd
+
+bank = int(input('How much money do you have:'))
+inp = bank
+bet = 1
+
+winning_list = []
+
+
+for x in range(365):
+    winnings = 0
+
+    bet = 1
+    bank = inp
+
+    for i in range(50):
+        if bet > bank:
+            print("BANKRUPT")
+
+
+            print()
+            break
+        else:
+            choice = random.randint(19, 36)
+            result = random.randint(0, 36)
+
+            if result <= 18:
+                bank -= bet
+                winnings -= bet
+
+
+
+                bet = bet * 2
+            else:
+                bank += bet
+                winnings += bet
+
+
+                bet = 1
+
+    winning_list.append(winnings)
+
+print("You have made", winning_list, "dollars")
+data = pd.DataFrame({'winnings': [winning_list]})
+datatoexcel = pd.ExcelWriter("data casino.xlsx", engine='xlsxwriter')
+data.to_excel(datatoexcel, sheet_name="Sheet1")
+datatoexcel.save()
